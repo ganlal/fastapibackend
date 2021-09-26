7f00f5d4f45ab00e42b8b4b7e97a074457aa6939
ganlal <ganlal@yahoo.com.au>
2021-09-26 16:47:20 +1300

    Lesson 54 Auth Part -2

--- webapps/auth/route_login.py
+++ webapps/auth/route_login.py
@@ -1,5 +1,5 @@
-from webapps.auth.forms import load_data
-from fastapi import APIRouter,Request, Depends, responses, status
+from webapps.auth.forms import LoginForm
+from fastapi import APIRouter,Request, Depends, responses, status, HTTPException
 from fastapi.templating import  Jinja2Templates
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
@@ -7,6 +7,7 @@ from db.session import get_db
 from schemas.users import UserCreate
 from sqlalchemy.exc import IntegrityError
 from pydantic import ValidationError
+from apis.version1.route_login import login_for_access_token
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(include_in_schema=False)
@@ -14,4 +15,25 @@ router = APIRouter(include_in_schema=False)
 @router.get("/login/")
 def login(request: Request):
     return templates.TemplateResponse("auth/login.html", {"request":request})
+    
+
+@router.post("/login/")
+async def login(request: Request, db : Session = Depends(get_db)):
+    form = LoginForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        try:
+            form.__dict__.update(msg="Login successful")
+            response = templates.TemplateResponse("auth/login.html",form.__dict__)
+            login_for_access_token(response=response,form_data=form,db=db)
+            return response
+        except HTTPException:
+            form.__dict__.update(msg="")
+            form.__dict__.get("errors").append("Incorrect email / passowrd")
+            return templates.TemplateResponse("auth/login.html",form.__dict__)
+    return templates.TemplateResponse("auth/login.html",form.__dict__)
+
+
+
+
     
